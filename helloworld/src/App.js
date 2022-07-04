d9d907b0e0e94e59941f84ad851f5db14e422331
ajinkya kambe <ajinkyakambe@gmail.com>
2022-07-04 23:58:57 +0530

    UC 4 Event Binding

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.jpg'
 
 class App extends React.Component {
 
+  url = "https://www.bridgelabz.com/"
   constructor(){
     super();
     this.state = {
@@ -11,11 +12,18 @@ class App extends React.Component {
     }
   }
 
+  //On Click Function
+ onClick = ($event) => {
+  console.log("Save button is clicked",$event);
+  window.open(this.url,"_blank");
+ }
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} alt="The Bridgelabz Logo" />
+        <img src={logo} onClick = {this.onClick} 
+         alt="The Bridgelabz Logo" />
       </div>
     )
   }
