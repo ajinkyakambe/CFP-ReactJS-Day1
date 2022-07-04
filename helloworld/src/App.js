49bc7fc80f8bff10104711089d69ecb7e07a1203
ajinkya kambe <ajinkyakambe@gmail.com>
2022-07-05 00:39:29 +0530

    UC 7 complete text

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -35,13 +35,26 @@ class App extends React.Component {
   render() {
     return (
       <>
+      <div className="inner_wrapper">
       <div>
-        <h1>Hello {this.state.userName} from Bridgelabz.</h1>
+        <h1>Hello {this.state.userName} from Bridgelabz</h1>
         <img src={logo} onClick = {this.onClick} 
          alt="The Bridgelabz Logo" />
       </div>
       <div>
         <input onChange={this.onNameChange}/>
+      </div>
+      <p>     
+        BridgeLabz is an AWS recognised incubator and is solely focussed on solving tech employability.
+        Over the last 4 years, it has worked with 500+ top tech companies and 3500+ engineers have
+        been hired from Bridgelabz. It has labs in Mumbai and Bangalore. 
+        It has partnered with over 500+ companies, including Yatra, Capgemini, Meru Cabs, LafargeHolcim, JDA,
+        UrbanLadder, Fullerton India, and RBL bank. BridgeLabz won Business World Techtor Award
+        2020 and secured 2nd position at Global Edtech Startup Awards 2019, India. Also, was 
+        selected by the Maharashtra state government as Top 100 Startups 2019. BridgeLabz is also
+        the employability partner for ETCS 5.0 ( Economic Times Campus Stars) program which is connected to over 5000+ engineering colleges in India.
+
+      </p>
       </div>
       </>
     )
